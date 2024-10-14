4adb528a9838804eb2669af9e083d82fee532983
Heaven <gabrielheavenlydawn@gmail.com>
2024-10-14 17:00:35 +0100

    chore: fixing deployment issues

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
 import Logo from "../../../public/bitPension logo nbg.svg";
 import Facebook from "../../../public/facebook.svg";
